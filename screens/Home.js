8549e6cefe4e1d21ce67f91df20218dec78edcea
qcakaj <36235406+qcakaj@users.noreply.github.com>
2022-01-14 10:40:58 +0100

    modified home

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,20 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
-import Colors from '../constants/Colors';
 import { useTheme } from '@react-navigation/native';
+import { db } from '../constants/firebaseConfig';
+import { collection,setDoc, doc, onSnapshot, deleteDoc  } from 'firebase/firestore';
+import { useSelector  } from 'react-redux';
 
-const ListButton = ({ title, color, onPress, onDelete, onOptions }) => {
-    const { colors } = useTheme();
-
-
+const ListButton = ({ title, color, onPress, onDelete, onOptions,colors}) => {
+    
     return (
         <TouchableOpacity
             style={[styles.itemContainer, { backgroundColor: color }]}
             onPress={onPress}>
             <View>
-                <Text style={[styles.itemTitle,{color:colors.text }]}> {title} </Text>
+                <Text style={[styles.itemTitle, { color: colors.text }]}> {title} </Text>
             </View>
             <View style={{ flexDirection: 'row' }}>
                 <TouchableOpacity onPress={onOptions} disallowInterruption={true}>
@@ -28,8 +28,8 @@ const ListButton = ({ title, color, onPress, onDelete, onOptions }) => {
     );
 };
 
-const renderAddListIcon = (navigation, addItemToLists) => {
-    const {colors} =useTheme();
+const renderAddListIcon = (navigation, addItemToLists,colors) => {
+  
 
     return (
         <View style={{ flexDirection: "row" }}>
@@ -44,30 +44,62 @@ const renderAddListIcon = (navigation, addItemToLists) => {
 }
 export default ({ navigation }) => {
 
-    const [lists, setLists] = useState([
-        { title: "School", color: Colors.blue },
-        { title: "Work", color: Colors.green }
-    ]);
+    const [lists, setLists] = useState([]);
+    const userUid = useSelector(state => state.userReducer.userUid);
+    const { colors } = useTheme();
+    const listsRef = collection(db, "users", userUid, "lists");
 
-    const addItemToLists = (item) => {
-        lists.push(item);
-        setLists([...lists]);
-    }
-
-    const removeItemFromLists = (index) => {
-        lists.splice(index, 1);
-        setLists([...lists]);
-    }
-
-    const updateItemFromLists = (index, item) => {
-        lists[index] = item
-        setLists([...lists])
-    }
     useLayoutEffect(() => {
         navigation.setOptions({
-            headerRight: () => renderAddListIcon(navigation, addItemToLists)
+            headerRight: () => renderAddListIcon(navigation, addItemToLists, colors)
         });
     });
+        useEffect(() => {
+          const subscribetoItems =   onSnapshot(listsRef, (doc) => {
+                let items = doc.docs.map((doc)=> {
+                    const data = doc.data();
+                    data.id = doc.id;
+                    return data
+                })
+                items = items.sort((a, b) => {
+                    if (a.index < b.index) {
+                        return -1
+                    }
+                    if (a.index > b.index) {
+                        return 1
+                    }
+                    return 0
+                });
+                  setLists(items);
+            });
+
+            return () => subscribetoItems();
+    }, [])
+    const addItemToLists = ({title, color }) => {
+        const index = lists.length > 1 ? lists[lists.length - 1].index + 1 : 0 ;
+        const document = title ? doc(db,"users",userUid,"lists", title) : collection(db,"users",userUid,"lists");
+        setDoc(document,{title,color,index}).then(()=> {
+           console.log("Add new item");
+        })
+    }
+
+    const removeItemFromLists = (id) => {
+        // lists.splice(index, 1);
+        // setLists([...lists]);
+        //deleteDoc(doc(db, "cities", "DC"));
+        deleteDoc(doc(db,"users",userUid,"lists", id)).then(()=> {
+            console.log("removed");
+        })
+    }
+
+    const updateItemFromLists = (id, item) => {
+
+        setDoc(doc(db,"users",userUid,"lists", id),item).then(()=>{
+            console.log("id: ", id);
+            console.log("index: ", item.index);
+        })
+    }
+
     return (
         <View style={styles.container}>
             <FlatList
@@ -82,20 +114,21 @@ export default ({ navigation }) => {
                         }}><Text >Empty list</Text></View>
                     );
                 }}
-                renderItem={({ item: { title, color }, index }) => {
+                renderItem={({ item: { title, color ,id , index} }) => {
                     return (
                         <ListButton
                             title={title}
                             color={color}
-                            onPress={() => { navigation.navigate("Todo List", { title, color }) }}
-                            onDelete={() => removeItemFromLists(index)}
+                            colors={colors}
+                            onPress={() => { navigation.navigate("Todo List", { title, color, listId:id }) }}
+                            onDelete={() => removeItemFromLists(id)}
                             onOptions={() => {
                                 navigation.navigate(
                                     "Edit",
                                     {
                                         title,
                                         color,
-                                        saveChanges: (item) => updateItemFromLists(index, item)
+                                        saveChanges: (newItem) => updateItemFromLists(id, {index,...newItem})
                                     })
                             }
                             } />
